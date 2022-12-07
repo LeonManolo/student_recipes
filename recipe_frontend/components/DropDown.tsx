b2ae0abb619ffa64fc58c70ae342aab2cdaf93c0
Max Ollech <max.ollech@stud.hs-flensburg.de>
2022-12-07 17:04:40 +0100

    style changes recipe

--- recipe_frontend/components/DropDown.tsx
+++ recipe_frontend/components/DropDown.tsx
@@ -1,7 +1,7 @@
 export default function DropDown({ children, ...props }: any) {
     return (
         <div className="dropdown dropdown-hover">
-            <label tabIndex={0} className="btn m-1 bg-custom-second">Kategorie</label>
+            <label tabIndex={0} className="btn bg-custom-first">Kategorie</label>
             <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                 <li><a>Kuchen</a></li>
                 <li><a>Nudeln</a></li>
--- recipe_frontend/components/Tab.tsx
+++ recipe_frontend/components/Tab.tsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 export default function Tab() {
   const [tab, setTab] = useState(0)
   return (
-    <div className="tabs tabs-boxed p-2">
+    <div className="tabs tabs-boxed p-2 bg-base-100 flex-col">
       <a className={`tab ${tab == 0 ? "tab-active" : ""}`} onClick={() => setTab(0)}>Günstig</a>
       <a className={`tab ${tab == 1 ? "tab-active" : ""}`} onClick={() => setTab(1)}>Schnell</a>
       <a className={`tab ${tab == 2 ? "tab-active" : ""}`} onClick={() => setTab(2)}>Beliebt</a>
