c5789bebd59ae281b795023da819266925ed59ea
NiklasHargarter <niklashargarter@gmail.com>
2022-12-07 13:25:15 +0100

    Query for getting user with his recipes but still keeping fetchtype lazy. Implemented association table for recipe_category

--- recipe_backend/src/main/kotlin/de/hsflensburg/recipe_backend/users/UserRepository.kt
+++ recipe_backend/src/main/kotlin/de/hsflensburg/recipe_backend/users/UserRepository.kt
@@ -2,6 +2,8 @@ package de.hsflensburg.recipe_backend.users
 
 import de.hsflensburg.recipe_backend.users.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -10,4 +12,10 @@ interface UserRepository : JpaRepository<User, Long> {
     fun findByIdAndEmail(id: Long, email: String): User?
 
     fun findByEmail(email: String): User?
+
+    // return user object with usable list of recipes.
+    // needed when fetchtype is lazy.
+    // when only the user is wanted but not his recipes the normal findbyId function should be used
+    @Query("SELECT u FROM User u JOIN FETCH u.recipes WHERE u.id = :id")
+    fun findByIdWithRecipes(@Param("id") id: Long?): User?
 }
