4831af6836e53412b56b2d7c74758b7ab4ba56c2
Jaron Rieger <jaron.rieger@stud.hs-flensburg.de>
2022-12-09 14:33:56 +0100

    create-recipe added

---
+++ recipe_frontend/app/recipes/create-recipe/page.tsx
@@ -0,0 +1,13 @@
+import RecipeOverview from '../../../components/RecipeOverview';
+import RecipeTable from '../../../components/RecipeTable';
+import Image from 'next/image';
+import img from '../../../public/food/pizza3.png'
+import { FiClock } from "react-icons/fi";
+import { FiThumbsUp } from "react-icons/fi";
+import { HiFire } from "react-icons/hi";
+export default async function CreateRecipe() {
+  return (
+    <div>
+    </div>
+  );
+}
