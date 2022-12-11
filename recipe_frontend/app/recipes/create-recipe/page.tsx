b789f8dd66e66b14d9b0f81fb7ae884e74384ecc
Jaron Rieger <jaron.rieger@stud.hs-flensburg.de>
2022-12-11 16:22:17 +0100

    create-recipe page

--- recipe_frontend/app/recipes/create-recipe/page.tsx
+++ recipe_frontend/app/recipes/create-recipe/page.tsx
@@ -1,6 +1,68 @@
 export default async function CreateRecipe() {
   return (
-    <div>
+    <div className="grid place-content-center ...">
+      <h1 className="ml-10 mt-10">
+        Neues Rezept
+      </h1>
+      <ul>
+        <li className="ml-10 mt-10">
+          Name des Rezepts <input type="text" placeholder="Hier schreiben" className="input input-bordered w-full max-w-xs ml-10" />
+        </li>
+        <li className="ml-10 mt-10">
+          Kurzbeschreibung <input type="text" placeholder="Hier schreiben" className="input input-bordered w-full max-w-xs ml-10" />
+        </li>
+        <li className="ml-10 mt-10">
+          Preis (€) <input type="text" placeholder="Hier schreiben" className="input input-bordered w-full max-w-xs ml-10" />
+        </li>
+        <li className="ml-10 mt-10">
+          Dauer (min) <input type="text" placeholder="Hier schreiben" className="input input-bordered w-full max-w-xs ml-10" />
+        </li>
+        <li className="ml-10 mt-10">
+          Schwierigkeit <select className="select select-bordered w-full max-w-xs ml-10">
+            <option selected>Einfach</option>
+            <option>Mittel</option>
+            <option>Schwer</option>
+          </select>
+        </li>
+        <li className="ml-10 mt-10">
+          Kategorie <input type="checkbox" className="checkbox ml-10" /> Kuchen <input type="checkbox" className="checkbox ml-10" /> Nudeln <input type="checkbox" className="checkbox ml-10" /> Reis <input type="checkbox" className="checkbox ml-10" /> Fleisch <input type="checkbox" className="checkbox ml-10" /> Vegetarisch
+        </li>
+        <li className="ml-10 mt-10">
+          <div className="overflow-x-auto">
+            Zutaten
+            <table className="table border-solid border-2 border-black w-full max-w-xs ml-40">
+              <thead>
+                <tr>
+                  <th>Menge</th>
+                  <th>Zutat</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="hover">
+                  <td>1</td>
+                  <td>A</td>
+                </tr>
+                <tr className="hover">
+                  <td>2</td>
+                  <td>B</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </li>
+        <li className="ml-10 mt-10">
+          Bilder <input type="file" className="file-input file-input-bordered w-full max-w-xs ml-10" />
+        </li>
+        <li className="ml-10 mt-10">
+          Zubereitung <textarea className="textarea textarea-bordered ml-10" placeholder="Hier schreiben" />
+        </li>
+        <li>
+          <table>
+            <tr><td><div contentEditable>I am editable</div></td><td><div contentEditable>I am also editable</div></td></tr>
+            <tr><td>I am not editable</td></tr>
+          </table>
+        </li>
+      </ul>
     </div>
   );
 }
